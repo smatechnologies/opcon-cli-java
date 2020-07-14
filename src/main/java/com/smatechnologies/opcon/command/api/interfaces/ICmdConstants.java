8efec02dd9e793f25febdfca855ad45427180d04
Bertie van Hinsbergen <bvanhinsbergen@smasolutionsIT.com>
2020-07-14 11:11:23 +0200

    update version to 1.1.4

--- src/main/java/com/smatechnologies/opcon/command/api/interfaces/ICmdConstants.java
+++ src/main/java/com/smatechnologies/opcon/command/api/interfaces/ICmdConstants.java
@@ -37,7 +37,7 @@ public interface ICmdConstants {
 	public static final String TRUE = "True";
 	
 	// general
-	public static final String SOFTWARE_VERSION = "1.1.3";
+	public static final String SOFTWARE_VERSION = "1.1.4";
 	public static final String SYSTEM_USER_DIRECTORY = "user.dir";
 	public static final String CONFIG_FILE_NAME = "Connector.config";
 	public static final String LOG_DATE_TIME_FORMAT = "yyyyMMdd HH:mm:ss";
